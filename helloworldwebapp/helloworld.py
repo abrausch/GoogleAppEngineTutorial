87fc292c9449b883e3b9c6818bfca3632b3957dd
Alexander Brausch <abrausch@web.de>
2011-02-04 12:37:46 +0100

    first commit

---
+++ helloworldwebapp/helloworld.py
@@ -0,0 +1,17 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        """docstring for get"""
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp world!')
+
+application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+
+def main():    
+    run_wsgi_app(application)
+    
+if __name__ == "__main__":
+    main()
+        
